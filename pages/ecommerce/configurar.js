594351ccd1c7e4101aeae0870f70c6f795cecf9f
Gabriel Benitez <gabriel@1tienda.online>
2021-01-16 18:37:50 -0300

    asd

---
+++ pages/ecommerce/configurar.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function pago() {
+  return (
+    <>
+      <Head>
+        <title>Configurar E-Commerce | 1tiendaonline</title>
+        <meta name="robots" content="noindex" />
+        <meta
+          name="description"
+          content="Configurá los datos de tu e-commerce."
+        />
+        <link rel="stylesheet" href="/ecommerce.css"></link>
+      </Head>
+      <section className="configurar">
+      </section>
+    </>
+  );
+}
